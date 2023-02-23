c009843da0dbfc2ed77373e05c9aee0c04a15000
Vladas <vladas.budvytis@gmail.com>
2023-02-23 18:03:42 +0200

    lesson2

---
+++ 01_31/antra_uzd.py
@@ -0,0 +1,9 @@
+sentence_one = input ("Please, write a sentence: ")
+
+print()
+print("Atvirksciai:")
+print(sentence_one[::-1])
+
+print()
+print("Kas antra raide:")
+print(sentence_one[::2])
---
+++ 01_31/pirma_uzd.py
@@ -0,0 +1,7 @@
+first_name = input ("What is your name? ")
+
+age = input("How old are you:" )
+
+birth_date = 2023 - int(age)
+
+print(f"Your name: {first_name}, you born {birth_date}")
